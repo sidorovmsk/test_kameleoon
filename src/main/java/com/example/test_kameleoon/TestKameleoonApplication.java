54c8d10a7bbfb9144edd392f510fd843363d16b6
a.v.sidorov <a.v.sidorov@itfbgroup.ru>
2023-12-06 19:37:43 +0300

    Initial commit

---
+++ src/main/java/com/example/test_kameleoon/TestKameleoonApplication.java
@@ -0,0 +1,13 @@
+package com.example.test_kameleoon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestKameleoonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestKameleoonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/test_kameleoon/TestKameleoonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.test_kameleoon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestKameleoonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
